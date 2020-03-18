493c64be097e54b931841dd14224f9a5d57a528b
Максим Гиль <lol700@live.com>
2020-03-18 15:35:54 +0300

    init commit

---
+++ DEV-1/DEV-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace DEV_1
+{
+    class Program
+    {/// <summary>
+     /// Input string and get results
+     /// </summary>
+     /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            var sequenceLenght = new SequenceLength(inputString);
+            sequenceLenght.SequenceGetCounter();
+            Console.WriteLine(sequenceLenght.ToString());
+
+        }
+
+       
+    }
+}
