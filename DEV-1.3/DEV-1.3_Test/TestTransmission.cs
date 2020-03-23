0935e217d8f787d05b482051686de7cf5b58c474
Maxim Gil <gil00maksim@gmail.com>
2020-03-23 16:02:41 +0300

    Add Test file

---
+++ DEV-1.3/DEV-1.3_Test/TestTransmission.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using DEV_1._3;
+using Assert = NUnit.Framework.Assert;
+using System;
+
+namespace DEV_1._3_Test
+{
+    [TestClass]
+    class TestTransmission
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        [DataRow("ZDADJB#34", 8, "ADAR1234AD")]
+        [DataRow("ZDADJB#34", 12, "ADAR12%$AD")]
+        [DataRow("ZDADJB#34", 12, "ADAR1%^4AD")]
+        public void TestInputValuesIfStringHasInvalidSymbols(string typeOfTransmission, int numberOfGears, string manufacturer)
+        {
+            var actual = new Transmission(typeOfTransmission, numberOfGears, manufacturer);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [DataRow("ZDADJB#34", -1, "ADAR1234AD")]
+
+        public void TestInputNegativeValues(string typeOfTransmission, int numberOfGears, string manufacturer)
+        {
+            var actual = new Transmission(typeOfTransmission, numberOfGears, manufacturer);
+        }
+    }
+}
