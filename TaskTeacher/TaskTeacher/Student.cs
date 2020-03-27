80451c34b45989d700daa207b864f9a0c4a03dce
Maxim Gil <gil00maksim@gmail.com>
2020-03-27 21:35:26 +0300

    minor update

--- TaskTeacher/TaskTeacher/Student.cs
+++ TaskTeacher/TaskTeacher/Student.cs
@@ -7,7 +7,9 @@ namespace TaskTeacher
 {
     class Student : ITaskPerformer
     {
-
+        readonly int resultLength = 15;
+        readonly int minLetterCount = 97;
+        readonly int maxLetterCount = 123;
         public string firstName { get; private set; }
         public string lastName { get; private set; }
         public delegate void StudentHandler(object sender, EventTaskArgs e);
@@ -18,14 +20,17 @@ namespace TaskTeacher
             this.firstName = firstName;
             this.lastName = lastName;
         }
-
+        /// <summary>
+        /// get string with random letters and give notify
+        /// </summary>
+        /// <returns>string with symbols</returns>
         public string DoTask()
         {
 
             StringBuilder result = new StringBuilder();
-            for (int i = 1; i < 15; i++)
+            for (int i = 1; i < resultLength; i++)
             {
-                char symbol = (char)Randomizer.rnd.Next(97, 123);
+                char symbol = (char)Randomizer.rnd.Next(minLetterCount, maxLetterCount);
                 result.Append(symbol);
             }
             TeacherNotify?.Invoke(this, new EventTaskArgs(result.ToString()));
